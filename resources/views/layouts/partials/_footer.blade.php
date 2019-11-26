14638acd9ae663f60452be81387c34407ae5f7d8
emmanuel aka <emmanuelaka2010@gmail.com>
2019-11-26 14:05:19 +0000

    Setup and pages statics

---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="text-center">
+   <p> &copy; {{date('Y')}} &middot; Laracarte by <a href="https://twitter.com/etsmo">@etsmo</a>
+   - A <a href="https://laramap.com">Laramap</a> clone App.
+</p>
+   <p>
+       <b>This App has been built for learning purposes.</b>
+    </p>
+</footer>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.default')
+
+@section('content')
+<div class="container">
+    <h2>What is Laracarte?</h2>
+    <p>Laracarte is a clone app of
+        &dash; <a href="https://laramap.com" target="_blank">Laramap.com</a>
+    </p>
+    <div class="row">
+        <div class="col-md-6">
+            <p class="alert alert-warning">
+                <strong><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> This App has been built by <a href="https://twitter.com/etsmo">@etsmo</a> for learning purposes.</strong>
+            </p>
+        </div>
+    </div>
+
+    <p>Fell free to help to improve the <a href="#">source code</a>.</p>
+    <hr>
+
+    <h2>What is Laramap?</h2>
+    <p>Laramap is the website by which Laracarte was inspired :)</p>
+    <p>
+        More info <a href="https://laramap.com.p.about">here</a>
+    </p>
+
+    <hr>
+
+    <h2>Which tools and services are used in Laracarte?</h2>
+    <p>
+       Basically it's built in Laravel &amp; Bootstrap. But there's a bunch of services and used for email and other sections.
+    </p>
+
+    <ul>
+        <li>Larabel</li>
+        <li>Bootstrap</li>
+        <li>Amazon S3</li>
+        <li>Mandrill</li>
+        <li>Google Map</li>
+        <li>Gravatar</li>
+        <li>Antony Martin's Geolocation package</li>
+        <li>Michael Fortin's Markdown Parser Package</li>
+        <li>Heroku</li>
+    </ul>
+</div>
+@endsection
